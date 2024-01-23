6e22045fc31fc1aaf137480488c7dd2e90917dec
ISOLATEDMAN <samudralakartikeya@gmail.com>
2024-01-23 06:13:50 +0530

    Second_change

--- Downloads/Where_is_my_bus-master/lib/login.dart
+++ Downloads/Where_is_my_bus-master/lib/login.dart
@@ -10,7 +10,7 @@ class Log_ extends StatefulWidget {
 }
 
 class _Log_State extends State<Log_> {
-  TextEditingController _studentIdController = TextEditingController();
+  TextEditingController _conformController = TextEditingController();
   TextEditingController _passwordController = TextEditingController();
   TextEditingController _emailController = TextEditingController();
   @override
@@ -29,7 +29,7 @@ class _Log_State extends State<Log_> {
             child:SingleChildScrollView(
             child: Container(
               width: 400,
-              height: 500,
+              height: 520,
               decoration:const BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.all(
@@ -63,29 +63,11 @@ Container(
   height: 55,
   child: TextField(
     cursorWidth: 2,
-    controller: _studentIdController,
-    decoration: InputDecoration(
-      filled: true,
-      fillColor: Color.fromARGB(159, 210, 208, 208),
-      labelText: "Student ID",
-      border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(30),
-      ),
-    ),
-  ),
-),
-SizedBox(height: 30),
-Container(
-  width: 300,
-  height: 55,
-  child: TextField(
-    
-    cursorWidth: 2,
     controller: _emailController,
     decoration: InputDecoration(
       filled: true,
       fillColor: Color.fromARGB(159, 210, 208, 208),
-      labelText: "EMAIL",
+      labelText: "Email",
       border: OutlineInputBorder(
         borderRadius: BorderRadius.circular(30),
       ),
@@ -110,11 +92,38 @@ Container(
     ),
   ),
 ),
+SizedBox(height: 30),
+Container(
+  width: 300,
+  height: 55,
+  child: TextField(
+    
+    cursorWidth: 2,
+    controller: _conformController,
+    decoration: InputDecoration(
+      filled: true,
+      fillColor: Color.fromARGB(159, 210, 208, 208),
+      labelText: "Confirm Password",
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(30),
+      ),
+    ),
+  ),
+),
 const SizedBox(height: 35),
 
 ElevatedButton( 
   onPressed: (){
-    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>Way_typ()),);
+    Navigator.push(
+      context,PageRouteBuilder(pageBuilder: (context, animation, secondaryAnimation) =>Way_typ(),
+                transitionsBuilder: (context, animation, secondaryAnimation, child) {
+                const begin = 0.0;
+                const end = 5.0;
+                var tween = Tween(begin: begin, end: end);
+                var fadeAnimation = animation.drive(tween);
+                return FadeTransition(opacity: fadeAnimation, child: child);
+                },
+      ),);
   }, 
   child:Text(
     "LOGIN",style: TextStyle(
@@ -134,6 +143,27 @@ SizedBox(height: 5),
 Container(
   child:TextButton(onPressed:(){},child: Text("Forgot Password?"),)
 ),
+InkWell(
+  onTap: () {
+    // Your onTap logic here
+  },
+  child: ClipOval(
+    child: Container(
+      color: Colors.transparent, // Set the background color to make the circular boundary visible
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Image.asset('assets/g_1.jpg', width: 26, height: 26,),
+          SizedBox(width: 5,),
+          Text("Sign up with Google"),
+        ],
+      ),
+      width: 250,
+      height: 30,
+    ),
+  ),
+)
+
 
                 ],
               ),
